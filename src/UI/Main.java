30f12631ed4810981c27acddcea3c781d4b5cf5b
SOLRing0 <sanchez@oleary.com>
2019-09-13 13:25:16 -0600

    added piece return

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,11 +1,16 @@
 package UI;
 
+import Pieces.Piece;
 import Player.Player;
+import Board.newPieces;
 
 public class Main {
     public static void main(String[] args) {
         Player player1 = new Player(true);
         Player player2 = new Player(false);
+        Piece[][] gamePieces = newPieces.newGame();
+        System.out.println(gamePieces);
+
 
     
     }
